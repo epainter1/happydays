cef915a044d08a41efff18bcd90bed2974021939
epainter1 <epainter2005@hotmail.com>
2015-10-29 19:48:22 -0400

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,4 @@
 var gulp   = require('gulp'),
-<<<<<<< HEAD
-	sass = require('gulp-sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	concat = require('gulp-concat'),
-	browserSync = require('browser-sync'),
-	reload = browserSync.reload;
-
-gulp.task('bs', function(){
-	browserSync.init({
-		proxy:'http://localhost/mySpecialDay/'
-	});
-});
-
-
-=======
 		sass = require('gulp-sass'),
 		autoprefixer = require('gulp-autoprefixer'),
 		concat = require('gulp-concat'),
@@ -26,7 +11,6 @@ gulp.task('bs', function() {
 	});
 });
 
->>>>>>> 7d081e1c0d0a599167548fd819033e5db2afe346
 gulp.task('default', ['bs', 'styles', 'watch']);
 
 gulp.task('styles', function() {
@@ -37,19 +21,11 @@ gulp.task('styles', function() {
 		}))
 		.pipe(concat('style.css'))
 		.pipe(gulp.dest('.'))
-<<<<<<< HEAD
-		.pipe(reload({stream:true}));
-=======
 		.pipe(reload({stream: true}));
->>>>>>> 7d081e1c0d0a599167548fd819033e5db2afe346
 });
 
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function() {
 	gulp.watch('sass/**/*.scss', ['styles']);
-<<<<<<< HEAD
-	gulp.watch('**/*.php',reload);
-=======
 	gulp.watch('**/*.php', reload);
->>>>>>> 7d081e1c0d0a599167548fd819033e5db2afe346
 });
--- page.php
+++ page.php
@@ -1,9 +1,16 @@
-<?php get_header();  ?>
-
 <div class="main">
   <div class="container">
-
+	<div class="header">
+		<h1>
+		    <?php the_field('spouseone') ?> &amp; <?php the_field('spousetwo') ?>
+		</h1>
+		<h3>
+		    <?php the_field('weddingdate') ?>
+		</h3>
+		<?php get_header();  ?>
+	</div>
     <div class="content">
+
       <?php // Start the loop ?>
       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
